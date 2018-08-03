2e2b8868f7031bfc1ed38752ca777fa3b5782f94
Chris Swimmer <chris@Chriss-MacBook-Air.local>
2018-08-02 20:23:29 -0700

    Big Update round one

--- index.js
+++ index.js
@@ -22,3 +22,25 @@ setInterval(function(){
   document.getElementById("weird").innerHTML = colorArray.join('');
   colorArray = [];
 },700);
+
+function clickHide() {
+  var stepArr = ["#stepOne","#stepTwo","#stepThree","#stepFour","#stepFive"];
+  var stepLongArr = ["#stepLongOne","#stepLongTwo","#stepLongThree","#stepLongFour","#stepLongFive"];
+
+  var x = "#" + event.target.id;
+  var i;
+  for(i=0;i<stepArr.length;i++){
+    console.log(stepArr[i]);
+    if(stepArr[i] === x){
+      console.log('in the if statement');
+      $(x).addClass("active-li");
+      $(stepLongArr[i]).addClass("display-long")
+      $(stepLongArr[i]).removeClass("display-hide");
+    }
+    else {
+      $(stepArr[i]).removeClass("active-li");
+      $(stepLongArr[i]).removeClass("display-long");
+      $(stepLongArr[i]).addClass("display-hide");
+    }
+  }
+}
